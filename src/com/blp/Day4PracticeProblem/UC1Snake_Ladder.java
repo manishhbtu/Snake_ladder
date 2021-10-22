df653c3eb81fe0fe54322173dd0bf306b3aca6de
manish <manishkmar772@gmail.com>
2021-10-22 12:31:03 +0530

    added:Welcom Snake and ladder

---
+++ src/com/blp/Day4PracticeProblem/UC1Snake_Ladder.java
@@ -0,0 +1,9 @@
+package com.blp.Day4PracticeProblem;
+
+public class UC1Snake_Ladder {
+    public static void main(String[] args) {
+        int position =0;
+        System.out.println("position of the player ="+position);
+
+    }
+}
