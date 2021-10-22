7878f1c51b4c50d0cba08e33359e1a7d8ad8d734
manish <manishkmar772@gmail.com>
2021-10-22 13:59:44 +0530

    Added:Factorial_Number

---
+++ src/com/blp/Day4PracticeProblem/Factorial.java
@@ -0,0 +1,16 @@
+package com.blp.Day4PracticeProblem;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number:" );
+        int no = sc.nextInt();
+        int fact = 1;
+        for (int i=1 ; i<=no; i++){
+            fact = fact*i;
+        }
+        System.out.println("factorial of number is :"+fact);
+    }
+}
